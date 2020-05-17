4fdcbc87a998efba82d9306544dda398fc1b4df4
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-05-17 14:27:09 +0200

    Created Basic Structure and init repo

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import '../sass/main.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+	return <h1>Hello</h1>
+}
+
+
+ReactDOM.render(
+	<App/>,
+	document.getElementById('app')
+)
