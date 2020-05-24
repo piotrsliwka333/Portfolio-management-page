35df02840f96e556f2d388a8c88f84b3de665dac
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-05-24 19:21:15 +0200

    Added CV

--- src/js/components/Footer/Footer.js
+++ src/js/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export const Footer =() => {
 	return (
 		<footer className='contact' id='contact'>
 			<h6 className='contact__title'>Contact</h6>
-			<a className='contact__git-hub' href='#'>
+			<a className='contact__git-hub' href='https://github.com/piotrsliwka333'>
 				<i className="fab fa-github"/>
 			</a>
 			<div className='container'>
@@ -14,22 +14,18 @@ export const Footer =() => {
 					<div className='col-10 col-md-5 col-xl-5 contact-box'>
 						<p className='contact-box__info'>Email: <span className='contact-box__info__bold'>piotrsliwka333@gmail.com</span></p>
 						<p className='contact-box__info'>Full Name: <span className={'contact-box__info__bold'}>Piotr Śliwka</span></p>
-						<p className='contact-box__info'>Download </p>
+						<p className='contact-box__info' >Download <a className='contact-box__info__bold' href='../../../images/CV_Piotr_Śliwka%20(2).pdf' download>CV</a></p>
 					</div>
 					<div className='col-10 col-md-5 col-xl-5 contact-box'>
-						<a className='contact-box__social' href='#'>
+						<a className='contact-box__social' target='_blank' href='https://www.linkedin.com/in/piotr-%C5%9Bliwka/'>
 							<i className="fab fa-linkedin"/>
 						</a>
-						<a className='contact-box__social' href='#'>
+						<a className='contact-box__social' target='_blank' href='https://www.facebook.com/piotr.sliwka.1272/'>
 							<i className="fab fa-facebook-square"/>
-						</a>
-						<a className='contact-box__social' href='#'>
-							Pracuj.pl
 						</a>
 					</div>
 				</div>
 			</div>
-
 		</footer>
 	)
 }
